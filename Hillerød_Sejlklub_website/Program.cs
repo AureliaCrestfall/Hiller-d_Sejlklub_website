a054335b10b791f89b91d6b4eb2ccd5cd23235a1
crestfall <mas025@edu.zealand.dk>
2025-04-29 22:18:56 +0200

    added builder to program

--- Hillerød_Sejlklub_website/Program.cs
+++ Hillerød_Sejlklub_website/Program.cs
@@ -1,3 +1,6 @@
+using Hillerød_Sejlklub_website.Repository;
+using Hillerød_Sejlklub_website.Service;
+
 namespace Hillerød_Sejlklub_website
 {
     public class Program
@@ -6,6 +9,14 @@ namespace Hillerød_Sejlklub_website
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddSingleton<IMemberRepo,MemberRepo>();
+            builder.Services.AddSingleton<MemberService>();
+            builder.Services.AddSingleton<IBlogRepo, BlogRepo>();
+            builder.Services.AddSingleton<BlogService>();
+            builder.Services.AddSingleton<IEventRepo, EventRepo>();
+            builder.Services.AddSingleton<EventService>();
+            builder.Services.AddSingleton<IBoatRepo, BoatRepo>();
+            builder.Services.AddSingleton<BoatService>();
             // Add services to the container.
             builder.Services.AddRazorPages();
 
