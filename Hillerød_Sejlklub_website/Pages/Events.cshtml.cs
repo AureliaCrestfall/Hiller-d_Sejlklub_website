961a2724ce0ef7eb87e8887ddc9ed19cab11ac4b
Carina43 <cag003@edu.zealand.dk>
2025-05-01 12:44:56 +0200

    rettet i events

--- Hillerød_Sejlklub_website/Pages/Events.cshtml.cs
+++ Hillerød_Sejlklub_website/Pages/Events.cshtml.cs
@@ -1,4 +1,5 @@
 using Hillerød_Sejlklub_website.Model;
+using Hillerød_Sejlklub_website.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,9 +7,21 @@ namespace Hillerød_Sejlklub_website.Pages
 {
     public class EventsModel : PageModel
     {
+        private readonly EventService _eventure;
 
+        public EventsModel(EventService eventure2)
+        {
+            _eventure = eventure2;
+            eventure = new Event();
+        }
 
         public Event eventure { get; set; }
+
+        public IActionResult OnPost()
+        {
+            _eventure.Add(eventure);
+            return RedirectToPage("/Events");
+        }
 
         public void OnGet()
         {
